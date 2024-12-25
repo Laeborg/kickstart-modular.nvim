661bc94bc8128a2b9974cc66bdbc28186d7143e2
Jonas Læborg <github@lbrg.dk>
2024-12-26 00:24:15 +0100

    add harpoon, moveline and edit keymaps

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -6,34 +6,34 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup()
 
-    -- Keybindings
-    vim.keymap.set('n', '<leader>a', function()
-      harpoon:list():append()
-    end)
-    vim.keymap.set('n', '<C-e>', function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    -- -- Keybindings
+    -- vim.keymap.set('n', '<leader>a', function()
+    --   harpoon:list():append()
+    -- end)
+    -- vim.keymap.set('n', '<C-e>', function()
+    --   harpoon.ui:toggle_quick_menu(harpoon:list())
+    -- end)
 
-    -- Nav til filer
-    vim.keymap.set('n', '<C-h>', function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set('n', '<C-j>', function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set('n', '<C-k>', function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set('n', '<C-l>', function()
-      harpoon:list():select(4)
-    end)
+    -- -- Nav til filer
+    -- vim.keymap.set('n', '<C-h>', function()
+    --   harpoon:list():select(1)
+    -- end)
+    -- vim.keymap.set('n', '<C-j>', function()
+    --   harpoon:list():select(2)
+    -- end)
+    -- vim.keymap.set('n', '<C-k>', function()
+    --   harpoon:list():select(3)
+    -- end)
+    -- vim.keymap.set('n', '<C-l>', function()
+    --   harpoon:list():select(4)
+    -- end)
 
-    -- Toggle prev/next
-    vim.keymap.set('n', '<C-S-P>', function()
-      harpoon:list():prev()
-    end)
-    vim.keymap.set('n', '<C-S-N>', function()
-      harpoon:list():next()
-    end)
+    --     -- Toggle prev/next
+    --     vim.keymap.set('n', '<C-S-P>', function()
+    --       harpoon:list():prev()
+    --     end)
+    --     vim.keymap.set('n', '<C-S-N>', function()
+    --       harpoon:list():next()
+    --     end)
   end,
 }
